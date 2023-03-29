5d22ec20779fb8a99b67099eb4e37ca0973eeb06
yschoo0 <yyssms@daum.net>
2023-03-29 18:42:10 +0900

    Udemy_JS_let

---
+++ script.js
@@ -0,0 +1,24 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "YiHyeon";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+
+let Person = 'jonas'
+let PI = 3.1415;
+console.log(PI);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'teacher';
+console.log(myFirstJob)
